2016f2b0a4c97268df70cf546b794f256c2111f6
devrodrigobnu <devrodrigobnu@gmail.com>
2023-08-07 13:12:20 -0300

    update
    exercise 11

--- aula_1_and_2/exercicios/exercicio_11.py
+++ aula_1_and_2/exercicios/exercicio_11.py
@@ -7,8 +7,25 @@
 
 import os
 
+def recebe_numeros_usuario():
+    numeros = input('Digite os números inteiros separados por espaço: ')
+    numeros_lista = [int(num) for num in numeros.split()]
+    return numeros_lista
+    
+def calcula_media_numero(numeros):
+    soma = sum(numeros)
+    media = soma / len(numeros)
+    return media
 
+def exibe_calculo_media(media):
+    print('A média é:', media)
+
+def main():
+    numeros = recebe_numeros_usuario()
+    media = calcula_media_numero(numeros)
+    exibe_calculo_media(media)
 
 
 if __name__ == '__main__':
     os.system('cls')
+    main()  
