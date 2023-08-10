7b0ab614c0c05d966c064fa4f1f0f18728c713e0
devrodrigobnu <devrodrigobnu@gmail.com>
2023-08-10 00:04:48 -0300

    Exercícios 8 ao 17
    Acrescentado lista de exercícios para estudos.

---
+++ aula_3_and_4/exercicios/exercicio_08.py
@@ -0,0 +1,7 @@
+# 1 - Classe Produto:
+# Crie uma classe chamada Produto que represente um produto em uma loja. 
+# A classe deve ter os atributos: nome (private), preco
+# e codigo (public). Crie um construtor para inicializar esses atributos e 
+# métodos get_nome(), get_preco() e set_preco(novo_preco) para
+# acessar e modificar o preço."
+
